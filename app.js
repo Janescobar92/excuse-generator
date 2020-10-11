d0d03ed27aaf4f87ba22b3f4ac55739124db1908
Janescobar92 <jancore92@gmail.com>
2020-10-11 22:18:19 +0000

    server-py-created

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var who = ["An ufo ","Noa ","A virus ","Lord Vader "];
-
-var action = ["stole ","killed ","kidnapped ","melted "];
-
-var what = ["my cofee ", "a hobbit ","a civilian ", "my neighboors house "];
-
-var when = ["after school. ", "yesterday. ", "this morning. ", "at the ragnarok. "];
-
 function excuseGnerator(){
-    var i = Math.floor(Math.random()*4);
-    var r = Math.floor(Math.random()*4);
-    var e = Math.floor(Math.random()*4);
-    var j = Math.floor(Math.random()*4);
+    let who = ["An ufo ","Noa ","A virus ","Lord Vader "];
+    
+    let action = ["stole ","killed ","kidnapped ","melted "];
+    
+    let what = ["my cofee ", "a hobbit ","a civilian ", "my neighboors house "];
+    
+    let when = ["after school. ", "yesterday. ", "this morning. ", "at the ragnarok. "];
+    
+    let i = Math.floor(Math.random()*4);
+    let r = Math.floor(Math.random()*4);
+    let e = Math.floor(Math.random()*4);
+    let j = Math.floor(Math.random()*4);
+    
     return who[i] + action[e] + what[r] + when[j];  
 }
 document.write(excuseGnerator());
